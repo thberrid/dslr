3684cc6b5075f424682f9a918a2bc0a5b7392f18
thierry <thberrid@student.42.fr>
2021-05-25 17:41:09 +0200

    wip

--- histogram.r
+++ histogram.r
@@ -11,87 +11,25 @@ if (!exists(c("dslr"))){
 
 #####
 
-features.indexes <- c(7:length(dslr))
-houses.names <- c("Ravenclaw", "Slytherin", "Gryffindor", "Hufflepuff")
+# ggplot(dslr, aes(x=Astronomy, color = "Hogwarts House")) + 
+#   geom_histogram()
 
-dslr.means <- outer(
-                    houses.names,
-                    features.indexes,
-                    Vectorize(function(x, y){
-                        ft_sum(dslr[dslr[2] == x, y])
-                    })
-            )
-rownames(dslr.means) <- houses.names
-colnames(dslr.means) <- colnames(dslr.describe)
-#dslr.means
+library(data.table)
 
-#hist(dslr[dslr[2] == "Ravenclaw", 16])
+train <- fread("datasets/dataset_train.csv")
 
-ggplot(dslr, aes(binwidth = 1)) + 
-#    geom_histogram(aes(x = Astronomy), fill="orange", alpha = .2) +
-#    geom_histogram(aes(x = Arithmancy), fill="blue", alpha = .2) + 
-    geom_histogram(aes(x = Herbology), fill="green", alpha = .2)
-#    + geom_histogram(fill="blue", alpha = .2)
+#p <- ggplot(train, aes(x=Astronomy, color = train$"Hogwarts House")) +
+#    geom_histogram(fill="white")q
+
+p <- ggplot(train, aes(x=notes, color = train$"Hogwarts House")) +
+    geom_histogram(position = "identity", alpha = 0.3)
 
 
-# variance
-# V <- function (X) {
-#     n <- ft_count(X)
-#     X <- ft_normalize(X)
-#     mean <- ft_mean(X)
-#     1 / n * sum((X - mean) ^ 2)
-# }
+p
 
-# dslr.means.variance <- apply(dslr.means, MARGIN = 2, FUN = V)
-# dslr.means.variance
-
-#hist(dslr.means.variance)
-
-
-#####
+#ggplot(train, aes(x=Divination))+
+#    geom_histogram(color="black", fill="white")+
+#   facet_grid(train$`Hogwarts House` ~ .)
 
 message("Press Enter to close")
 invisible(readLines("stdin", n=1))
-
-
-#  library(data.table)
-# library(ggplot2)
-
-# train <- fread("/Users/camilleberrido/tobi/datasets/dataset_train.csv")
-
-# gplot(train, aes(x=Divination))+
-#   +     geom_histogram(color="black", fill="white")+
-#   +     facet_grid(train$`Hogwarts House` ~ .)
-
-
-
-# Camille Berrido
-# 21:37
-# ggplot(train, aes(x=Astronomy, color = train$"Hogwarts House")) + 
-# +     geom_histogram()
-# Camille Berrido
-# 21:43
-# ggplot(train, aes(x=Divination))+
-# +     geom_histogram(color="black", fill="white")+
-# +     facet_grid(train$`Hogwarts House` ~ .)
-# Camille Berrido
-# 21:46
-#  library(data.table)
-# library(ggplot2)
-
-# train <- fread("/Users/camilleberrido/tobi/datasets/dataset_train.csv")
-
-# gplot(train, aes(x=Divination))+
-#   +     geom_histogram(color="black", fill="white")+
-#   +     facet_grid(train$`Hogwarts House` ~ .)
-# Camille Berrido
-# 21:48
-# dplyr
-# Vous
-# 21:50
-# https://github.com/thberrid/dslr
-# Camille Berrido
-# 22:02
-# https://medium.com/coinmonks/dealing-with-missing-data-using-r-3ae428da2d1u7
-
-:
